77ed193674f4a6b2d1bc321ab702aac57db1a0b1
Berlinda Anaman <berlinda.anaman@amalitech.org>
2021-06-04 18:03:12 +0000

    Built functionality of the BMI calculator

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-function calcBodyMass(weight,height) {
-    
-    var BMI = Math.round (weight/(height**2));
-    
-if (BMI < 18.5) {
-     return "Your BMI is " + BMI + ", so you are underweight.";
+let button = document.getElementById("btn");
+
+function BMI() {
+     let height = document.getElementById("height").value;
+     let weight = document.getElementById("weight").value;
+
+     if (height === '' || height <= 0) {
+          document.getElementById("results").innerHTML =
+            "Height cannot be empty nor a negative value";
+     }
+     else if (weight === '' || weight <= 0) {
+          document.getElementById("results").innerHTML =
+            "Weight cannot be empty nor a negative value";
+     }
+     else {
+          let bmi = Math.round(weight / (height ** 2));
+
+          if (bmi < 18.5) {
+               document.getElementById("results").innerHTML =
+                    "Your BMI is " + bmi + ", so you are underweight.";
+          }
+          else if (bmi > 18.5 && BMI < 24.9) {
+               document.getElementById("results").innerHTML =
+                    "Your BMI is " + bmi + ", so you have a normal weight.";
+          }
+          else if (bmi > 24.9) {
+               document.getElementById("results").innerHTML =
+                    "Your BMI is " + bmi + ", so you are overweight.";
+          }
+     }
 }
-if (BMI > 18.5 && BMI < 24.9) {
-     return "Your BMI is " + BMI + ", so you have a normal weight.";
-}
-if (BMI > 24.9) {
-     return "Your BMI is " + BMI + ", so you are overweight.";
-}
-}
+
+   button.addEventListener ('click', BMI) 
